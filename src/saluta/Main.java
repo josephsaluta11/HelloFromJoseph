67402aaea9a8974746c2c85755352dd7829e4ec4
Jefferson Reyes <reyes.jefferson1412@gmail.com>
2021-10-09 17:31:04 +0800

    Added new line of string.

--- src/saluta/Main.java
+++ src/saluta/Main.java
@@ -1,12 +1,11 @@
 package saluta;
 
-
-
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello, my name is Joseph");
+        System.out.println("Hello Joseph Saluta, my name is Jefferson Reyes.");
 
     }
 }
